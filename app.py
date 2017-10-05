dc7edaf5349d3cc0d75f6a12d8704e886211ed8e
Jordan Hand <jordanhand@comcast.net>
2017-10-04 19:09:32 -0700

    added python backend

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/api")
+def api():
+    return "API"
+
+@app.route('/js/<path:path>')
+def js(path):
+    return send_from_directory("public/js", "%s.js" % path)
+
+@app.route('/css/<path:path>')
+def js(path):
+    return send_from_directory("public/css", "%s.css" % path)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    if path == "":
+        return send_from_directory("public", "index.html")
+    else:
+        return send_from_directory("public/views", "%s.html" % path)
+
+if __name__ == "__main__":
+    app.run()
