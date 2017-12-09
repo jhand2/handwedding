1f90bc57d66d683ce62e265b433454f336c721a9
Jordan Hand <jordanhand22@gmail.com>
2017-12-09 10:51:05 -0800

    change pathing to be cross platform

--- main/__init__.py
+++ main/__init__.py
@@ -26,7 +26,7 @@ def rsvp():
 
 @app.route("/api/photo_paths")
 def photo_paths():
-    return json.dumps(os.listdir("public/img/engagement/jpg"))
+    return json.dumps(os.listdir(os.path.join("public", "img", "engagement", "jpg")))
 
 @app.route('/js/<path:path>')
 def js(path):
