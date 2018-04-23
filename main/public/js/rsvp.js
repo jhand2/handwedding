38ed5bd625b521f42a63a80c7f9d76fabe0e4319
Jordan Hand <jordanhand22@gmail.com>
2018-04-22 21:10:04 -0700

    remove rsvp content

--- main/public/js/rsvp.js
+++ main/public/js/rsvp.js
@@ -1,52 +1,52 @@
-$(function() {
-    var height = $(".navbar").height();
-    var fields = $(".form-group").length;
-    $(".container").css("margin-top", height);
+//$(function() {
+    //var height = $(".navbar").height();
+    //var fields = $(".form-group").length;
+    //$(".container").css("margin-top", height);
 
-    $("#add-guest").on("click", function(e) {
-        $("#guest-container").append(
-            '<div>' +
-                '<input class="guest-row form-control"></input>' +
-                '<i class="fa fa-times-circle fa-2x" aria-hidden="true"></i>' +
-            '</div>'
-        );
+    //$("#add-guest").on("click", function(e) {
+        //$("#guest-container").append(
+            //'<div>' +
+                //'<input class="guest-row form-control"></input>' +
+                //'<i class="fa fa-times-circle fa-2x" aria-hidden="true"></i>' +
+            //'</div>'
+        //);
 
-        $(".fa-times-circle").on("click", function(e) {
-            var i = $(this).parent().index();
-            $(this).parent().remove();
-        });
-    });
+        //$(".fa-times-circle").on("click", function(e) {
+            //var i = $(this).parent().index();
+            //$(this).parent().remove();
+        //});
+    //});
 
-    var submit_data = function() {
-        var going_selected = $('input[name=optradio]:checked', '#rsvp_submit').val();
-        var guests = [];
-        $(".guest-row").each(function(i) {
-            guests.push($(this).val());
-        });
+    //var submit_data = function() {
+        //var going_selected = $('input[name=optradio]:checked', '#rsvp_submit').val();
+        //var guests = [];
+        //$(".guest-row").each(function(i) {
+            //guests.push($(this).val());
+        //});
 
-        var d = {
-            "name": $("#name").val(),
-            "attending": going_selected == "true",
-            "guests": guests
-        }
+        //var d = {
+            //"name": $("#name").val(),
+            //"attending": going_selected == "true",
+            //"guests": guests
+        //}
 
-        $.ajax({
-            url: "/api/rsvp",
-            type: "post",
-            data: JSON.stringify(d),
-            processData: false,
-            contentType: "application/json"
-        }).then(function() {
-            $("#name").val("");
-            $("input").blur();
-            $("#guest-container").empty();
-            $('#rsvp input[type="radio":checked]').each(function(){
-                $(this).checked = false;  
-            });
-        });
-    }
+        //$.ajax({
+            //url: "/api/rsvp",
+            //type: "post",
+            //data: JSON.stringify(d),
+            //processData: false,
+            //contentType: "application/json"
+        //}).then(function() {
+            //$("#name").val("");
+            //$("input").blur();
+            //$("#guest-container").empty();
+            //$('#rsvp input[type="radio":checked]').each(function(){
+                //$(this).checked = false;  
+            //});
+        //});
+    //}
 
-    $("#submit").on("click", function(e) {
-        submit_data();
-    });
-})
+    //$("#submit").on("click", function(e) {
+        //submit_data();
+    //});
+//})
